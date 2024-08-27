3db5b658d46f7c43f74df8e3c838e4b55f3cb268
SM Ashiq <ashiqsm.du@gmail.com>
2024-08-27 21:56:49 +0600

    feat: Add stack methods for push, pop, is_empty, size, peek, and display

--- stack.py
+++ stack.py
@@ -1,4 +1,25 @@
-class Stack:
-    def __init__(self, items):
-        self.items
+class Stack():
+    def __init__(self):
+        self.items = []
         
+    def push(self, item):
+        self.items.append(item)
+        
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise Exception("Stack is empty")
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def display(self):
+        print(self.items)
